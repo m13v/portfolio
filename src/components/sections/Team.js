6a0855f6161910e74e3fc6e475cbd64332af7189
matthew-heartful <matthew.heartful@gmail.com>
2024-06-26 18:40:32 -0700

    new project added, two commented out

--- src/components/sections/Team.js
+++ src/components/sections/Team.js
@@ -129,6 +129,7 @@ const Team = () => {
     <Section id="team">
       <Title>Recent Projects</Title>
       <Container>
+        <MemberComponent link="https://www.linkedin.com/posts/matthew-diakonov-a84a1911_made-a-search-assistant-this-weekend-for-activity-7205835625688670210-pdKg?utm_source=share&utm_medium=member_desktop" name="AI Sales Assistant" position="An assistant that helps users find products. Rendering a React carousel through function calls for an e-commerce product catalog. The product search is multi-modal, allowing for context-based image searches" />
         <MemberComponent link="https://www.linkedin.com/posts/matthew-diakonov-a84a1911_just-launched-an-autodebugger-a-typical-activity-7204346521654366208-4Iye?utm_source=share&utm_medium=member_desktop" name="GPT Auto Debugger" position="Coding assistant that actually test runs the code in a safe sandbox environment." />
         <MemberComponent link="https://www.linkedin.com/posts/matthew-diakonov-a84a1911_new-cli-interface-for-httpslnkdingsnrwjf-activity-7194409697230168066-l95n?utm_source=share&utm_medium=member_desktop" name="Gemini 4 Docs" position="Search and chat with any documentation inside your terminal." />
         <MemberComponent link="https://www.linkedin.com/posts/matthew-diakonov-a84a1911_played-with-sd-lightning-voice2img-its-activity-7186174839504748544-8tAI?utm_source=share&utm_medium=member_desktop" name="Realtime Sora" position="Gamified immersive image generation. Control prompts with keyboard shortcuts. SD Lightning, 15 frames/sec." />
@@ -144,9 +145,9 @@ const Team = () => {
         <MemberComponent link="https://www.linkedin.com/posts/matthew-diakonov-a84a1911_put-clothes-on-models-using-ai-to-make-a-activity-7142589170014633984-fBON?utm_source=share&utm_medium=member_desktop" name="Virtual Try-On" position="Uses AI to put clothes on models, making a merchant's life easier." />
         <MemberComponent link="https://youtu.be/DNqqRRQaLk8" name="Image Gen Tool" position="1 million visitors in 3 months, 25k MAU, $10k MRR." />
         <MemberComponent link="https://www.linkedin.com/posts/matthew-diakonov-a84a1911_coliving-startuphouse-techhouse-activity-7071924035613323264-P8bJ?utm_source=share&utm_medium=member_desktop" name="Feliciti" position="Matching platform for residents and co-living houses." />
-        <MemberComponent link="https://docs.google.com/presentation/d/1QHevVRvSCvuMJFhgY0s4LkvGccCuWHyCwts8PAKeRBY/edit?usp=sharing" name="Warm Intro Network" position="Peer-to-peer referral system for hiring." />
+        {/* <MemberComponent link="https://docs.google.com/presentation/d/1QHevVRvSCvuMJFhgY0s4LkvGccCuWHyCwts8PAKeRBY/edit?usp=sharing" name="Warm Intro Network" position="Peer-to-peer referral system for hiring." /> */}
         <MemberComponent link="https://docs.google.com/presentation/d/17HxnFUF7jhE1Bc0C9W31on75dFszVZFll0JGBfVZ0-4/edit?usp=sharing" name="GetQ" position="Cloud compute. Templates for your frontend and backend." />
-        <MemberComponent link="https://docs.google.com/presentation/d/1hMSQ4dAnNEaQnzM8osHKzjVEDmaRsWGtHY2m_aFKn0M/edit?usp=sharing" name="WEB3 Developer Leaderboard" position="Links GitHub contributions to Dune Analytics performance of web3 protocols." />
+        {/* <MemberComponent link="https://docs.google.com/presentation/d/1hMSQ4dAnNEaQnzM8osHKzjVEDmaRsWGtHY2m_aFKn0M/edit?usp=sharing" name="WEB3 Developer Leaderboard" position="Links GitHub contributions to Dune Analytics performance of web3 protocols." /> */}
         <MemberComponent link="https://youtu.be/WKirA7P59fQ" name="Betzy" position="Bet on your good habits with friends." />
         <MemberComponent link="https://docs.google.com/presentation/d/1OuZb8LslpVTa4_Hp8aRvs1enDjZ-lQ8FJ5BilJmM_vo/edit?usp=sharing" name="TimeTree" position="Time tracker for personal productivity." />
         <MemberComponent link="https://youtu.be/jGbqnAjLi-0" name="Newcomers" position="Matchmaking among newcomers and locals for mutual help." />
